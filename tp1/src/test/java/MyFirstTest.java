9b92de54a7bbea50cbf1e4e49bf2d4f4023e181d
Tn3m3lc <clem-braillon@hotmail.fr>
2017-04-05 16:46:59 +0200

    Ajout classe test

---
+++ tp1/src/test/java/MyFirstTest.java
@@ -0,0 +1,34 @@
+//import com.sun.istack.internal.logging.Logger;
+import java.util.logging.Logger;
+import org.junit.*;
+
+public class MyFirstTest {
+
+	private static Logger log = Logger.getLogger(MyFirstTest.class.getSimpleName());
+	
+	@BeforeClass
+	public static void firstInit() {log.info("Première méthode appelée");}
+	
+	@AfterClass
+	public static void lastDestroy() {log.info("Dernière méthode appelée");}
+	
+	@Before
+	public void beforeTest() {log.info("Méthode appelée avant chaque test");}
+	
+	@After
+	public void afterTest() {log.info("Méthode appelée après chaque test");}
+	
+	@Test
+	public void premierTest() {
+		log.info("Mon premier test");
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void secondTest() {
+		log.info("Mon deuxième test");
+		Assert.assertFalse(false);
+	}
+	
+}
+
